8c916510840dbd4cbd879861ba1a8c4f72e333fb
AfredVon <shanejinru@gmail.com>
2016-09-17 18:47:32 +0800

    modified functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -5,29 +5,29 @@ import unittest
 class NewVisitor(unittest.TestCase):
 
     def setUp(self):
-        self.brower = webdriver.Firefox()
-        self.brower.implicitly_wait(3)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
-        self.brower.quit()
+        self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.brower.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪斯听说有个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.brower.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
         
         #她注意到网页的标题和头部都包含“To-Do"这个词
-        self.assertIn('To-Do', self.brower.title)
-        header_text = self.brower.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         
         #应用邀她输入一个待办事项
-        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
@@ -45,13 +45,13 @@ class NewVisitor(unittest.TestCase):
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly(”使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
-        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新,她的清单中显示了这两个待办事项 
-        self.check_for_row_in_list_table('1 :Buy peacock feathers')
-        self.check_for_row_in_list_table('2 :Use peacock feather to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
         # 伊迪丝想知道这个网站是否会记住她的清单
